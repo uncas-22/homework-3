737219bdd287e65fb963719bbabab7c006751073
Alex <seredin-alex@mail.ru>
2022-09-21 20:45:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("homework-3 exercise 1");
+        {
+            //write your code here
+            double dog = 8;
+            System.out.println("dog"); System.out.println(dog);
+            var cat = 3.6;
+            System.out.println("cat"); System.out.println(cat);
+            double paper = 763789;
+            System.out.println("paper"); System.out.println(paper);
+        System.out.println("homework-3 exercise 2");
+            //write your code here
+            dog = dog + 4;
+            System.out.println("dog"); System.out.println(dog);
+            cat = cat + 4;
+            System.out.println("cat"); System.out.println(cat);
+            paper = paper +4;
+            System.out.println("paper"); System.out.println(paper);
+        System.out.println("homework-3 exercise 3");
+            //write your code here
+            dog = dog - 3.5;
+            System.out.println("dog"); System.out.println(dog);
+            cat = cat - 1.6;
+            System.out.println("cat"); System.out.println(cat);
+            paper = paper - 7639;
+            System.out.println("paper"); System.out.println(paper);
+        System.out.println("homework-3 exercise 4");
+            var friend = 19;
+            System.out.println("friend"); System.out.println(friend);
+            friend = friend * 2;
+            System.out.println("friend"); System.out.println(friend);
+            friend = friend / 7;
+            System.out.println("friend"); System.out.println(friend);
+
+        }
+        }
+    }
